fbf55e5a4b2310a0e35a522f8e8e252b91a10298
Noah Friedman <noahfriedman2@gmail.com>
2022-08-18 16:33:48 -0400

    Define interface for RotatableGridVector::rotate

--- RotatableGridVector.hpp
+++ RotatableGridVector.hpp
@@ -6,7 +6,19 @@ class RotatableGridVector : public std::vector<std::vector<T>> {
     using std::vector<std::vector<T>>::vector;
 
     public:
+        /**
+         * A move constructor to allow conversion between this class and its parent class.
+         * @param rhs The data to be moved into this object.
+         */
         explicit RotatableGridVector(std::vector<std::vector<T>> &&rhs) : std::vector<std::vector<T>>(rhs) {}
+
+        /**
+         * Rotates the contents of this object 90º clockwise.
+         * @warning Behaviour is undefined when the internal vectors do not all have the same length.
+         */
+        void rotate() {
+            throw std::runtime_error("not yet implemented");
+        }
 };
 
 #endif //ROTATABLEGRIDVECTOR_HPP
