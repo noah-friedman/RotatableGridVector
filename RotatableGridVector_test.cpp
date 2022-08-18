75cbf7c14151fcc9ecfdb795625e3ed5da6398e2
Noah Friedman <noahfriedman2@gmail.com>
2022-08-18 16:58:56 -0400

    Add tests for RotatableGridVector [skip ci]

--- RotatableGridVector_test.cpp
+++ RotatableGridVector_test.cpp
@@ -1,2 +1,48 @@
 #include <gtest/gtest.h>
 #include "RotatableGridVector.hpp"
+
+#define GEN                                                                                                             \
+if (rows * cols > 26) {                                                                                                 \
+    std::stringstream e;                                                                                                \
+    e << "generation size must be less than 26, but " << rows << "(rows) * " << cols << "(columns) = " << rows * cols;  \
+    throw std::out_of_range(e.str());                                                                                   \
+}                                                                                                                       \
+RotatableGridVector<unsigned char> r(rows, decltype(r)::value_type(cols))
+
+RotatableGridVector<unsigned char> genActual(size_t rows, size_t cols) {
+    GEN;
+
+    unsigned char c = 'A';
+    for ( decltype(r)::value_type &row : r )
+        for ( unsigned char &v : row )
+            v = c++;
+
+    return r;
+}
+
+RotatableGridVector<unsigned char> genExpected(size_t rows, size_t cols) {
+    GEN;
+
+    unsigned char c = 'A';
+    for (int col = (int)--cols; col >= 0; col--) {
+        for (int row = 0; row < rows; row++) {
+            r[row][col] = c++;
+        }
+    }
+
+    return r;
+}
+
+#define TEST_(ROWS, COLS)                           \
+TEST(RotatableGridVectorTest, ROWS ## x ## COLS) {  \
+    auto actual = genActual(ROWS, COLS);            \
+    actual.rotate();                                \
+    ASSERT_EQ(actual, genExpected(ROWS, COLS));     \
+} static_assert(true)
+
+TEST_(5, 5);
+TEST_(4, 4);
+TEST_(3, 5);
+TEST_(5, 3);
+TEST_(4, 6);
+TEST_(6, 4);
